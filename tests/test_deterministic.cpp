4ea705d4111184e2ee95ae75a37707cbf925ae46
Oskar Jansson <oskja067@student.liu.se>
2018-04-10 16:13:08 +0200

    updates: test now reflects major update

--- tests/test_deterministic.cpp
+++ tests/test_deterministic.cpp
@@ -1,47 +1,48 @@
 #include <gtest/gtest.h>
-#include "server.h"
 #include "client.h"
-#include "package.h"
+#include "message.h"
+#include "inbox.h"
 
 struct PackageTest : testing::Test {
-	Server* server;
-	Client* client;
+	Inbox<Client>* inbox;
+	Client* alice;
 
 	PackageTest() {
-		server = new Server();
-		client = new Client(*server);
+		inbox = new Inbox<Client>();
+		alice = new Client("Alice", *inbox);
 	}
 	virtual ~PackageTest() {
-		delete server;
-		delete client;
+		delete alice;
+		delete inbox;
 	}
 };
 
 
 TEST_F(PackageTest, Creation) {
-	ASSERT_TRUE(server != nullptr);
+	ASSERT_TRUE(inbox != nullptr);
+	ASSERT_TRUE(alice != nullptr);
 }
 
 
-TEST_F(PackageTest, SendSome) {
-  client -> send_packages(1); 
-  std::vector<Package> compare{Package(0)}; 
-  ASSERT_EQ(server -> get_log(), compare); 
-
-  std::vector<Package> compare2{Package(0),
-                                Package(0), 
-                                Package(1),
-                                Package(2),
-                                Package(3),
-                                Package(4),
-                                Package(5),
-                                Package(6),
-                                Package(7),
-                                Package(8),
-                                Package(9)}; 
-  client -> send_packages(10);
-  EXPECT_EQ(server -> get_log(), compare2); 
-
-} 
+// TEST_F(PackageTest, SendSome) {
+//   client -> send_packages(1); 
+//   std::vector<Package> compare{Package(0)}; 
+//   ASSERT_EQ(server -> get_log(), compare); 
+// 
+//   std::vector<Package> compare2{Package(0),
+//                                 Package(0), 
+//                                 Package(1),
+//                                 Package(2),
+//                                 Package(3),
+//                                 Package(4),
+//                                 Package(5),
+//                                 Package(6),
+//                                 Package(7),
+//                                 Package(8),
+//                                 Package(9)}; 
+//   client -> send_packages(10);
+//   EXPECT_EQ(server -> get_log(), compare2); 
+// 
+// } 
 
 
