5ee0d17a8e10b683074fadb6ca9e4a9919323c62
Oskar Jansson <oskja067@student.liu.se>
2018-04-11 16:27:33 +0200

    updates: syntax and var name

--- taste/inbox.h
+++ taste/inbox.h
@@ -1,17 +1,18 @@
-#ifndef INBOX_H 
-#define INBOX_H 
+#ifndef INBOX_H
+#define INBOX_H
 
-#include "message.h"
 #include <map>
+#include "message.h"
 
-template <typename T> 
+template <typename T>
 class Inbox {
-  public: 
-    Inbox();
-    void enroll(T & client);
-    void recv(Message & msg);
-  private:
-    std::map<std::string, T*> routing_table;  
+ public:
+  Inbox();
+  void enroll(T& recipient);
+  virtual void recv(Message& msg);
+
+ private:
+  std::map<std::string, T*> routing_table;
 };
 #include "inbox.impl"
 #endif
