87c16b391f02ad87ab891a8702d7499c59a78f38
Oskar Jansson <oskja067@student.liu.se>
2018-04-11 16:28:53 +0200

    updates: test to use stub

--- tests/test_deterministic.cpp
+++ tests/test_deterministic.cpp
@@ -1,48 +1,36 @@
 #include <gtest/gtest.h>
-#include "client.h"
-#include "message.h"
-#include "inbox.h"
+
+#include "../taste/client.h"
+#include "../taste/message.h"
+#include "stubs/inbox_stub.h"
+#include "unordered.h"
 
 struct PackageTest : testing::Test {
-	Inbox<Client>* inbox;
-	Client* alice;
+  InboxStub<Client>* inbox;
+  Client* alice;
+  Client* bob;
 
-	PackageTest() {
-		inbox = new Inbox<Client>();
-		alice = new Client("Alice", *inbox);
-	}
-	virtual ~PackageTest() {
-		delete alice;
-		delete inbox;
-	}
+  PackageTest() {
+    inbox = new InboxStub<Client>{};
+    alice = new Client("Alice", *dynamic_cast<Inbox<Client>*>(inbox));
+    bob = new Client("Bob", *dynamic_cast<Inbox<Client>*>(inbox));
+
+    inbox->enroll(*alice);
+    inbox->enroll(*bob);
+  }
+  virtual ~PackageTest() {
+    delete alice;
+    delete bob;
+    delete inbox;
+  }
 };
 
-
 TEST_F(PackageTest, Creation) {
-	ASSERT_TRUE(inbox != nullptr);
-	ASSERT_TRUE(alice != nullptr);
+  ASSERT_TRUE(inbox != nullptr);
+  ASSERT_TRUE(alice != nullptr);
 }
 
-
-// TEST_F(PackageTest, SendSome) {
-//   client -> send_packages(1); 
-//   std::vector<Package> compare{Package(0)}; 
-//   ASSERT_EQ(server -> get_log(), compare); 
-// 
-//   std::vector<Package> compare2{Package(0),
-//                                 Package(0), 
-//                                 Package(1),
-//                                 Package(2),
-//                                 Package(3),
-//                                 Package(4),
-//                                 Package(5),
-//                                 Package(6),
-//                                 Package(7),
-//                                 Package(8),
-//                                 Package(9)}; 
-//   client -> send_packages(10);
-//   EXPECT_EQ(server -> get_log(), compare2); 
-// 
-// } 
-
-
+TEST_F(PackageTest, SendSome) {
+  alice->send(*(new Message{"PING", alice->get_name(), bob->get_name()}));
+  ASSERT_MSG(*(new Message{"PONG", bob->get_name(), alice->get_name()}));
+}
