090154cfeb013740b8e43d5431845b704b79276e
Oskar Jansson <oskja067@student.liu.se>
2018-04-11 16:26:45 +0200

    updates: syntax style

--- taste/client.h
+++ taste/client.h
@@ -7,13 +7,14 @@
 
 class Client {
  public:
-  Client(std::string name, Inbox<Client> & inbox); 
-  void send(Message & msg);
-  void recv(Message & msg);
-  std::string get_name(); 
+  Client(std::string name, Inbox<Client>& inbox);
+  void send(Message& msg);
+  void recv(Message& msg);
+  std::string get_name();
+
  private:
   std::string name;
-  Inbox<Client> & inbox;
+  Inbox<Client>& inbox;
 };
 
 #endif
