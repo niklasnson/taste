593670086f9da36163106daaec5f054cc6f95347
Oskar Jansson <oskja067@student.liu.se>
2018-04-11 16:28:31 +0200

    adds: first macro:unordered commit

---
+++ tests/unordered.h
@@ -0,0 +1,9 @@
+#ifndef UNORDERED_H
+#define UNORDERED_H
+
+#define ASSERT_MSG(expected)                      \
+  do {                                            \
+    std::vector<Message>* log = inbox->get_log(); \
+  } while (false)
+
+#endif /* UNORDERED_H */
