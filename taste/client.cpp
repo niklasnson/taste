970d48c6fad9a041eb5bcc79cb52e2fe70b050df
Oskar Jansson <oskja067@student.liu.se>
2018-04-12 15:41:25 +0200

    updates: a sleep delay in recv

--- taste/client.cpp
+++ taste/client.cpp
@@ -1,28 +1,21 @@
 #include "client.h"
-#include "inbox.h"
+#include <chrono>
 #include <iostream>
+#include <thread>
+#include "inbox.h"
 
-Client::Client(std::string name, Inbox<Client> & inbox)
-  :name{name} ,inbox(inbox)
-  {}
+Client::Client(std::string name, Inbox<Client>& inbox)
+    : name{name}, inbox(inbox) {}
 
+void Client::send(Message& msg) { inbox.recv(msg); }
 
-void
-Client::send(Message & msg) {
-  inbox.recv(msg);
-}
-
-void
-Client::recv(Message & msg) {
-  std::cout << name << " rcv msg: " << msg.message << std::endl; 
+void Client::recv(Message& msg) {
+  std::cout << name << " rcv msg: " << msg.message << std::endl;
 
   if (msg.message == "PING") {
-    send(*(new Message{"PONG", name, msg.from})); 
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    send(*(new Message{"PONG", name, msg.from}));
   }
 }
 
-std::string
-Client::get_name() {
-  return name; 
-}
-
+std::string Client::get_name() { return name; }
