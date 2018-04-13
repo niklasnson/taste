c1d519c80da91ae96b0f466457d1ef1981336035
Oskar Jansson <oskja067@student.liu.se>
2018-04-13 13:31:47 +0200

    updates: inbox has locks for the log

--- tests/stubs/inbox_stub.h
+++ tests/stubs/inbox_stub.h
@@ -2,6 +2,7 @@
 #define INBOX_STUB_H
 
 #include <map>
+#include <mutex>
 #include <string>
 #include <vector>
 #include "../../taste/inbox.h"
@@ -13,15 +14,24 @@ class InboxStub : public Inbox<T> {
   InboxStub() = default;
   void recv(Message& msg) override;
   std::vector<Message>* get_log();
+  std::mutex* get_lock();
 
  private:
   std::vector<Message> log;
+  std::mutex log_lock;
 };
 
 template <typename T>
 void InboxStub<T>::recv(Message& msg) {
+  log_lock.lock();
   log.push_back(msg);
+  log_lock.unlock();
   Inbox<T>::recv(msg);
+}
+
+template <typename T>
+std::mutex* InboxStub<T>::get_lock() {
+  return &log_lock;
 }
 
 template <typename T>
