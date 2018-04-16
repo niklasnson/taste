4bb328cb3a8315fd0c95310595ec388d52f61998
Oskar Jansson <oskja067@student.liu.se>
2018-04-16 12:59:40 +0200

    removes: tests with timeouts

--- tests/test_deterministic.cpp
+++ tests/test_deterministic.cpp
@@ -44,15 +44,15 @@ TEST_F(PackageTest, SendOneMessage) {
   msg_thread.join();
 }
 
-TEST_F(PackageTest, SendOneMessageWithTimeout) {
-  std::thread msg_thread(
-      send_message,
-      std::ref(*(new Message{"PING", alice->get_name(), bob->get_name()})),
-      alice);
-  Message msg{"PONG", bob->get_name(), alice->get_name()};
-  EXPECT_MSG_TIMER(msg, 1.5);
-  msg_thread.join();
-}
+// TEST_F(PackageTest, SendOneMessageWithTimeout) {
+//   std::thread msg_thread(
+//       send_message,
+//       std::ref(*(new Message{"PING", alice->get_name(), bob->get_name()})),
+//       alice);
+//   Message msg{"PONG", bob->get_name(), alice->get_name()};
+//   EXPECT_MSG_TIMER(msg, 1.5);
+//   msg_thread.join();
+// }
 
 TEST_F(PackageTest, AssertUnordered) {
   Message tjena{"Tjena.", alice->get_name(), bob->get_name()};
@@ -68,29 +68,29 @@ TEST_F(PackageTest, AssertUnordered) {
   msg_thread3.join();
 }
 
-TEST_F(PackageTest, AssertUnorderedWithTimer) {
-  Message tjena{"Tjena.", alice->get_name(), bob->get_name()};
-  Message tja{"Tja.", bob->get_name(), alice->get_name()};
-  Message leget{"Läget?", alice->get_name(), bob->get_name()};
-  std::vector<Message> messages{tja, leget, tjena};
-  std::vector<std::thread> threads{};
+// TEST_F(PackageTest, AssertUnorderedWithTimer) {
+//   Message tjena{"Tjena.", alice->get_name(), bob->get_name()};
+//   Message tja{"Tja.", bob->get_name(), alice->get_name()};
+//   Message leget{"Läget?", alice->get_name(), bob->get_name()};
+//   std::vector<Message> messages{tja, leget, tjena};
+//   std::vector<std::thread> threads{};
 
-  for (int i{0}; i < 100; i++) {
-    Message annoying{"[" + std::to_string(i) + "]Are We There Yet?",
-                     bob->get_name(), alice->get_name()};
-    std::thread msg_thread(send_message, std::ref(annoying), bob);
-    threads.push_back(std::move(msg_thread));
-  }
-  EXPECT_UNORDERED_TIMER(messages, 0.0);
+//   for (int i{0}; i < 100; i++) {
+//     Message annoying{"[" + std::to_string(i) + "]Are We There Yet?",
+//                      bob->get_name(), alice->get_name()};
+//     std::thread msg_thread(send_message, std::ref(annoying), bob);
+//     threads.push_back(std::move(msg_thread));
+//   }
+//   EXPECT_UNORDERED_TIMER(messages, 0.0);
 
-  std::thread msg_thread1(send_message, std::ref(tjena), alice);
-  std::thread msg_thread2(send_message, std::ref(tja), alice);
-  std::thread msg_thread3(send_message, std::ref(leget), alice);
-  threads.push_back(std::move(msg_thread1));
-  threads.push_back(std::move(msg_thread2));
-  threads.push_back(std::move(msg_thread3));
-  for (auto it{threads.begin()}; it != threads.end(); ++it) it->join();
-}
+//   std::thread msg_thread1(send_message, std::ref(tjena), alice);
+//   std::thread msg_thread2(send_message, std::ref(tja), alice);
+//   std::thread msg_thread3(send_message, std::ref(leget), alice);
+//   threads.push_back(std::move(msg_thread1));
+//   threads.push_back(std::move(msg_thread2));
+//   threads.push_back(std::move(msg_thread3));
+//   for (auto it{threads.begin()}; it != threads.end(); ++it) it->join();
+// }
 
 TEST_F(PackageTest, SleepAssertMsg) {
   std::thread msg_thread(
@@ -128,25 +128,35 @@ TEST_F(PackageTest, SleepAssertUnordered) {
    */
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   auto log = inbox->get_log();
+
+  bool test{false};
   for (auto& msg : *log) {
     if (msg == tjena) {
       ASSERT_EQ(msg, tjena);
+      test = !test;
     }
   }
+  ASSERT_TRUE(test);
 
+  test = !test;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   for (auto& msg : *log) {
     if (msg == tja) {
       ASSERT_EQ(msg, tja);
+      test = !test;
     }
   }
+  ASSERT_TRUE(test);
 
+  test = !test;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   for (auto& msg : *log) {
     if (msg == leget) {
       ASSERT_EQ(msg, leget);
+      test = !test;
     }
   }
+  ASSERT_TRUE(test);
 
   threads.push_back(std::move(msg_thread1));
   threads.push_back(std::move(msg_thread2));
