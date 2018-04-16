625813156f026db0db680ac2f7998a9d33ac0e80
Oskar Jansson <oskja067@student.liu.se>
2018-04-16 13:07:01 +0200

    renames: test_determ.. to test_solutions

--- tests/test_solutions.cpp
+++ tests/test_solutions.cpp
@@ -27,11 +27,6 @@ struct PackageTest : testing::Test {
   }
 };
 
-TEST_F(PackageTest, Creation) {
-  ASSERT_TRUE(inbox != nullptr);
-  ASSERT_TRUE(alice != nullptr);
-}
-
 void send_message(Message& msg, Client* sender) { sender->send(msg); }
 
 TEST_F(PackageTest, SendOneMessage) {
@@ -40,59 +35,11 @@ TEST_F(PackageTest, SendOneMessage) {
       std::ref(*(new Message{"PING", alice->get_name(), bob->get_name()})),
       alice);
   Message msg{"PONG", bob->get_name(), alice->get_name()};
-  ASSERT_MSG(msg);
+  EXPECT_MSG(msg);
   msg_thread.join();
 }
 
-// TEST_F(PackageTest, SendOneMessageWithTimeout) {
-//   std::thread msg_thread(
-//       send_message,
-//       std::ref(*(new Message{"PING", alice->get_name(), bob->get_name()})),
-//       alice);
-//   Message msg{"PONG", bob->get_name(), alice->get_name()};
-//   EXPECT_MSG_TIMER(msg, 1.5);
-//   msg_thread.join();
-// }
-
-TEST_F(PackageTest, AssertUnordered) {
-  Message tjena{"Tjena.", alice->get_name(), bob->get_name()};
-  Message tja{"Tja.", bob->get_name(), alice->get_name()};
-  Message leget{"Läget?", alice->get_name(), bob->get_name()};
-  std::thread msg_thread1(send_message, std::ref(tjena), alice);
-  std::thread msg_thread2(send_message, std::ref(tja), alice);
-  std::thread msg_thread3(send_message, std::ref(leget), alice);
-  std::vector<Message> messages{tja, leget, tjena};
-  ASSERT_UNORDERED(messages);
-  msg_thread1.join();
-  msg_thread2.join();
-  msg_thread3.join();
-}
-
-// TEST_F(PackageTest, AssertUnorderedWithTimer) {
-//   Message tjena{"Tjena.", alice->get_name(), bob->get_name()};
-//   Message tja{"Tja.", bob->get_name(), alice->get_name()};
-//   Message leget{"Läget?", alice->get_name(), bob->get_name()};
-//   std::vector<Message> messages{tja, leget, tjena};
-//   std::vector<std::thread> threads{};
-
-//   for (int i{0}; i < 100; i++) {
-//     Message annoying{"[" + std::to_string(i) + "]Are We There Yet?",
-//                      bob->get_name(), alice->get_name()};
-//     std::thread msg_thread(send_message, std::ref(annoying), bob);
-//     threads.push_back(std::move(msg_thread));
-//   }
-//   EXPECT_UNORDERED_TIMER(messages, 0.0);
-
-//   std::thread msg_thread1(send_message, std::ref(tjena), alice);
-//   std::thread msg_thread2(send_message, std::ref(tja), alice);
-//   std::thread msg_thread3(send_message, std::ref(leget), alice);
-//   threads.push_back(std::move(msg_thread1));
-//   threads.push_back(std::move(msg_thread2));
-//   threads.push_back(std::move(msg_thread3));
-//   for (auto it{threads.begin()}; it != threads.end(); ++it) it->join();
-// }
-
-TEST_F(PackageTest, SleepAssertMsg) {
+TEST_F(PackageTest, SleepExpectMsg) {
   std::thread msg_thread(
       send_message,
       std::ref(*(new Message{"PING", alice->get_name(), bob->get_name()})),
@@ -101,12 +48,26 @@ TEST_F(PackageTest, SleepAssertMsg) {
 
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   auto log = inbox->get_log();
-  ASSERT_EQ(msg, log->back());
+  EXPECT_EQ(msg, log->back());
 
   msg_thread.join();
 }
 
-TEST_F(PackageTest, SleepAssertUnordered) {
+TEST_F(PackageTest, ExpectUnordered) {
+  Message tjena{"Tjena.", alice->get_name(), bob->get_name()};
+  Message tja{"Tja.", bob->get_name(), alice->get_name()};
+  Message leget{"Läget?", alice->get_name(), bob->get_name()};
+  std::thread msg_thread1(send_message, std::ref(tjena), alice);
+  std::thread msg_thread2(send_message, std::ref(tja), alice);
+  std::thread msg_thread3(send_message, std::ref(leget), alice);
+  std::vector<Message> messages{tja, leget, tjena};
+  EXPECT_UNORDERED(messages);
+  msg_thread1.join();
+  msg_thread2.join();
+  msg_thread3.join();
+}
+
+TEST_F(PackageTest, SleepExpectUnordered) {
   Message tjena{"Tjena.", alice->get_name(), bob->get_name()};
   Message tja{"Tja.", bob->get_name(), alice->get_name()};
   Message leget{"Läget?", alice->get_name(), bob->get_name()};
@@ -132,31 +93,31 @@ TEST_F(PackageTest, SleepAssertUnordered) {
   bool test{false};
   for (auto& msg : *log) {
     if (msg == tjena) {
-      ASSERT_EQ(msg, tjena);
+      EXPECT_EQ(msg, tjena);
       test = !test;
     }
   }
-  ASSERT_TRUE(test);
+  EXPECT_TRUE(test);
 
   test = !test;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   for (auto& msg : *log) {
     if (msg == tja) {
-      ASSERT_EQ(msg, tja);
+      EXPECT_EQ(msg, tja);
       test = !test;
     }
   }
-  ASSERT_TRUE(test);
+  EXPECT_TRUE(test);
 
   test = !test;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   for (auto& msg : *log) {
     if (msg == leget) {
-      ASSERT_EQ(msg, leget);
+      EXPECT_EQ(msg, leget);
       test = !test;
     }
   }
-  ASSERT_TRUE(test);
+  EXPECT_TRUE(test);
 
   threads.push_back(std::move(msg_thread1));
   threads.push_back(std::move(msg_thread2));
