26c2b1a95371005e58378015f55409906da53eaa
Oskar Jansson <oskja067@student.liu.se>
2018-04-16 12:59:22 +0200

    adds: a client now delays sending for a random amount of time

--- taste/client.cpp
+++ taste/client.cpp
@@ -4,14 +4,20 @@
 #include <thread>
 #include "inbox.h"
 
+#include <random>
+
 Client::Client(std::string name, Inbox<Client>& inbox)
     : name{name}, inbox(inbox) {}
 
-void Client::send(Message& msg) { inbox.recv(msg); }
+void Client::send(Message& msg) {
+  // std::random_device rd;
+  std::default_random_engine gen;
+  std::uniform_int_distribution<int> dist(1000, 10000);
+  std::this_thread::sleep_for(std::chrono::milliseconds(dist(gen)));
+  inbox.recv(msg);
+}
 
 void Client::recv(Message& msg) {
-  // std::cout << name << " rcv msg: " << msg.message << std::endl;
-
   if (msg.message == "PING") {
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     send(*(new Message{"PONG", name, msg.from}));
