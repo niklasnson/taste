739c93167a30645b731c8f22159fbd8515e0d2a9
Oskar Jansson <oskja067@student.liu.se>
2018-04-16 11:04:12 +0200

    adds: sleep-test

--- tests/test_deterministic.cpp
+++ tests/test_deterministic.cpp
@@ -92,4 +92,64 @@ TEST_F(PackageTest, AssertUnorderedWithTimer) {
   for (auto it{threads.begin()}; it != threads.end(); ++it) it->join();
 }
 
-TEST(Whatever, WaitingForTheSun) { ASSERT_EQ(true, true); }
+TEST_F(PackageTest, SleepAssertMsg) {
+  std::thread msg_thread(
+      send_message,
+      std::ref(*(new Message{"PING", alice->get_name(), bob->get_name()})),
+      alice);
+  Message msg{"PONG", bob->get_name(), alice->get_name()};
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+  auto log = inbox->get_log();
+  ASSERT_EQ(msg, log->back());
+
+  msg_thread.join();
+}
+
+TEST_F(PackageTest, SleepAssertUnordered) {
+  Message tjena{"Tjena.", alice->get_name(), bob->get_name()};
+  Message tja{"Tja.", bob->get_name(), alice->get_name()};
+  Message leget{"Läget?", alice->get_name(), bob->get_name()};
+  std::vector<Message> messages{tja, leget, tjena};
+  std::vector<std::thread> threads{};
+
+  for (int i{0}; i < 100; i++) {
+    Message annoying{"[" + std::to_string(i) + "]Are We There Yet?",
+                     bob->get_name(), alice->get_name()};
+    std::thread msg_thread(send_message, std::ref(annoying), bob);
+    threads.push_back(std::move(msg_thread));
+  }
+
+  std::thread msg_thread1(send_message, std::ref(tjena), alice);
+  std::thread msg_thread2(send_message, std::ref(tja), alice);
+  std::thread msg_thread3(send_message, std::ref(leget), alice);
+  /*
+   * Expecting things unordered with sleep
+   */
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  auto log = inbox->get_log();
+  for (auto& msg : *log) {
+    if (msg == tjena) {
+      ASSERT_EQ(msg, tjena);
+    }
+  }
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  for (auto& msg : *log) {
+    if (msg == tja) {
+      ASSERT_EQ(msg, tja);
+    }
+  }
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  for (auto& msg : *log) {
+    if (msg == leget) {
+      ASSERT_EQ(msg, leget);
+    }
+  }
+
+  threads.push_back(std::move(msg_thread1));
+  threads.push_back(std::move(msg_thread2));
+  threads.push_back(std::move(msg_thread3));
+  for (auto it{threads.begin()}; it != threads.end(); ++it) it->join();
+}
