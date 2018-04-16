b754b514adab550722607a0fc0936840c8661ee7
Oskar Jansson <oskja067@student.liu.se>
2018-04-16 09:56:39 +0200

    adds: comments

--- tests/unordered.h
+++ tests/unordered.h
@@ -3,10 +3,17 @@
 
 #include <chrono>
 
+/*!
+ * Pretty print messages.
+ */
 std::ostream& operator<<(std::ostream& os, Message const& m) {
   return os << "<" << m.message << " " << m.from << " " << m.to << ">";
 }
 
+/*!
+ * \brief A timer class where done() will return true if the timer has "gone
+ * off".
+ */
 class Timer {
  public:
   Timer(double max_seconds)
@@ -23,6 +30,9 @@ class Timer {
   }
 };
 
+/*!
+ * Returns the next incoming message for a specific receiver.
+ */
 Message& next_message(std::string recipient, std::vector<Message>* log) {
   while (log->empty()) {
     continue;
@@ -33,18 +43,29 @@ Message& next_message(std::string recipient, std::vector<Message>* log) {
   return log->back();
 }
 
+/*!
+ * Assert that the next message received by expected.to, is equal to expected.
+ */
 #define ASSERT_MSG(expected)                                \
   do {                                                      \
     std::vector<Message>* am_log = inbox->get_log();        \
     ASSERT_EQ(expected, next_message(expected.to, am_log)); \
   } while (false)
 
+/*!
+ * Expect that the next message received by expected.to, is equal to expected.
+ */
 #define EXPECT_MSG(expected)                                \
   do {                                                      \
     std::vector<Message>* am_log = inbox->get_log();        \
     EXPECT_EQ(expected, next_message(expected.to, am_log)); \
   } while (false)
 
+/*!
+ * Assert that the next message received by expected.to, is equal to expected.
+ * Function also takes a double for a timeout that fails the assertion if
+ * exceeded.
+ */
 #define ASSERT_MSG_TIMER(expected, timeout)                   \
   do {                                                        \
     std::vector<Message>* am_log = inbox->get_log();          \
@@ -64,6 +85,11 @@ Message& next_message(std::string recipient, std::vector<Message>* log) {
     ASSERT_EQ(expected, am_log->back());                      \
   } while (false)
 
+/*!
+ * Expect that the next message received by expected.to, is equal to expected.
+ * Function also takes a double for a timeout that fails the expect if
+ * exceeded.
+ */
 #define EXPECT_MSG_TIMER(expected, timeout)                   \
   do {                                                        \
     std::vector<Message>* am_log = inbox->get_log();          \
@@ -83,6 +109,9 @@ Message& next_message(std::string recipient, std::vector<Message>* log) {
     EXPECT_EQ(expected, am_log->back());                      \
   } while (false)
 
+/*!
+ * Parameter is a list containing messages to assert in an unordered fashion.
+ */
 #define ASSERT_UNORDERED(expects_list)                                 \
   do {                                                                 \
     std::vector<Message>* au_log = inbox->get_log();                   \
@@ -112,6 +141,43 @@ Message& next_message(std::string recipient, std::vector<Message>* log) {
     }                                                                  \
   } while (false)
 
+/*!
+ * Parameter is a list containing messages to expect in an unordered fashion.
+ */
+#define EXPECT_UNORDERED(expects_list)                                 \
+  do {                                                                 \
+    std::vector<Message>* au_log = inbox->get_log();                   \
+    std::mutex* au_log_lock = inbox->get_lock();                       \
+    while (au_log->empty()) {                                          \
+      continue;                                                        \
+    }                                                                  \
+    bool au_done{false};                                               \
+    while (!au_done) {                                                 \
+      for (auto it{expects_list.begin()}; it != expects_list.end();) { \
+        bool removed{false};                                           \
+        au_log_lock->lock();                                           \
+        for (auto& msg : *au_log) {                                    \
+          if (msg == *it) {                                            \
+            EXPECT_EQ(*it, msg);                                       \
+            it = expects_list.erase(it);                               \
+            removed = true;                                            \
+            break;                                                     \
+          }                                                            \
+        }                                                              \
+        au_log_lock->unlock();                                         \
+        if (!removed) {                                                \
+          ++it;                                                        \
+        }                                                              \
+      }                                                                \
+      if (expects_list.empty()) au_done = !au_done;                    \
+    }                                                                  \
+  } while (false)
+
+/*!
+ * First parameter is a vector containing messages to assert in an unordered
+ * fashion. Second is a double to which starts a timer and fails the assertion
+ * if reached.
+ */
 #define ASSERT_UNORDERED_TIMER(expects_list, timeout_ms)                  \
   do {                                                                    \
     Timer au_timer{timeout_ms};                                           \
@@ -150,6 +216,11 @@ Message& next_message(std::string recipient, std::vector<Message>* log) {
     }                                                                     \
   } while (false)
 
+/*!
+ * First parameter is a vector containing messages to expect in an unordered
+ * fashion. Second is a double to which starts a timer and fails the expect
+ * if reached.
+ */
 #define EXPECT_UNORDERED_TIMER(expects_list, timeout_ms)                  \
   do {                                                                    \
     Timer au_timer{timeout_ms};                                           \
