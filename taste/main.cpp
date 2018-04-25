40ec37b6abfcccb6d05401e3be7e553f19cab45f
Oskar Jansson <oskja067@student.liu.se>
2018-04-25 08:37:16 +0200

    fixes: /taste now makes

--- taste/main.cpp
+++ taste/main.cpp
@@ -1,21 +1,22 @@
-#include <iostream> 
+#include <iostream>
 #include "client.h"
 #include "inbox.h"
 #include "message.h"
 
 void usage(char* name) {
-  std::cout << std::endl << name << " [options]" << std::endl << 
-  "Options:" << std::endl;
+  std::cout << std::endl
+            << name << " [options]" << std::endl
+            << "Options:" << std::endl;
 }
 
-int main(int argc, char *argv[]) {
-
-  Inbox<Client> inbox; 
-  Client alice("Alice", inbox); 
-  Client bob("Bob", inbox); 
+int main(int argc, char* argv[]) {
+  Inbox<Client> inbox;
+  unsigned seed{1337};
+  Client alice("Alice", inbox, seed);
+  Client bob("Bob", inbox, seed);
   Message message("PING", "Alice", "Bob");
   inbox.enroll(alice);
   inbox.enroll(bob);
   alice.send(message);
-  return 0; 
+  return 0;
 }
