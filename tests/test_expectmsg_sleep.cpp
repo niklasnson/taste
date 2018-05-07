3dbd2e0534b88f8e5a11357adcd2f87fef28adc0
Oskar Jansson <oskja067@student.liu.se>
2018-05-07 16:35:32 +0200

    adds: one sleep test that does the trick

---
+++ tests/test_expectmsg_sleep.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <thread>
+
+#include "../taste/message.h"
+#include "../taste/taste.hpp"
+#include "arguments.h"
+#include "unordered.h"
+
+struct PackageTest : testing::Test {
+  Taste* taste;
+  PackageTest() {
+    std::vector<Message> messages{Message("PING", "Alice", "Bob"),
+                                  Message("PONG", "Bob", "Alice")};
+    taste = new Taste{messages, seed, 1000};
+  }
+
+  virtual ~PackageTest() {}
+};
+
+TEST_F(PackageTest, SleepExpectMsg) {
+  std::thread t{&Taste::run, taste};
+  t.detach();
+  // Use sleep to expect correct message
+  Message response{"PONG", "Bob", "Alice"};
+  std::this_thread::sleep_for(std::chrono::milliseconds(sleep_one));
+  auto log = taste->get_log();
+  EXPECT_EQ(response, log.back());
+}
