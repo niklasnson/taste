11c877295c48c0c1e619d4f81a467e5307a40d70
Oskar Jansson <oskja067@student.liu.se>
2018-05-08 09:54:03 +0200

    updates: now uses log pointer

---
+++ tests/test_expectmsg.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <thread>
+
+#include "../taste/message.h"
+#include "../taste/taste.hpp"
+#include "arguments.h"
+#include "unordered.h"
+
+struct PackageTest : testing::Test {
+  Taste* taste;
+  Message* fail;
+  PackageTest() {
+    fail = new Message{"Fail", "Fail", "Fail"};
+    std::vector<Message> messages{Message("PING", "Alice", "Bob"),
+                                  Message("PONG", "Bob", "Alice")};
+    taste = new Taste{messages, seed, 1000};
+  }
+
+  virtual ~PackageTest() {
+    delete taste;
+    delete fail;
+  }
+};
+
+TEST_F(PackageTest, SleepExpectMsg) {
+  std::thread t{&Taste::run, taste};
+  t.detach();
+
+  // Block until this message arrives.
+  Message response{"PONG", "Bob", "Alice"};
+  EXPECT_MSG(response);
+}
