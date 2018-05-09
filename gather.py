dd790320d5d2d841bf4d4cd0d0bb867acc7d57b2
Oskar Jansson <oskja067@student.liu.se>
2018-05-09 12:45:44 +0200

    updates: gather is now more efficient

--- gather.py
+++ gather.py
@@ -5,50 +5,78 @@ import subprocess
 import itertools
 
 
-def get_args(iterations):
-    # 18432
-    seeds = ["1337"]  # , "1989", "42", "420", "2018", "1804"]
-    n_messages = ["100"]  # , "1000", "1000000"]
-    sleeps = ["0", "50", "500", "1000"]
+def get_unordered():
+    seeds = ["1337", "4711", "42", "3571290", "1984"]
+    n_messages = ["0", "5", "10"]
     for seed in seeds:
         for n in n_messages:
-            for perm in list(itertools.product(sleeps, repeat=5)):
-                test_config = ["./test_solutions.out", seed, n]
-                test_config += perm
-                test_config += ["--gtest_repeat={}".format(iterations)]
+            test_config = ["./unordered.out", seed, n, "0"]
+            yield test_config
+
+
+def get_unordered_sleep():
+    seeds = ["1337", "4711", "42", "3571290", "1984"]
+    n_messages = ["0", "5", "10"]
+    sleeps = ["0", "500", "1000", "1500", "2000",
+              "2500", "3000", "3500", "4000", "4500", "5000"]
+    for seed in seeds:
+        for n in n_messages:
+            for sleep in sleeps:
+                test_config = ["./unordered_sleep.out", seed, n, sleep]
                 yield test_config
 
 
-def main(iterations):
-    col_width = 12
-    labels = ["seed", "n_messages", "sleep_one", "sleep_uno1",
-              "sleep_uno2", "sleep_uno3", "sleep_uno4"]
-    print("[=========]", "Gathering for", iterations, "iterations.")
+def get_expectmsg():
+    seeds = ["1337", "4711", "42", "3571290", "1984"]
+    for seed in seeds:
+        test_config = ["./expectmsg.out", seed, "0", "0"]
+        yield test_config
+
+
+def get_expectmsg_sleep():
+    seeds = ["1337", "4711", "42", "3571290", "1984"]
+    sleeps = ["0", "500", "1000", "1500", "2000",
+              "2500", "3000", "3500", "4000", "4500", "5000"]
+    for seed in seeds:
+        for sleep in sleeps:
+            test_config = ["./expectmsg_sleep.out", seed, "0", sleep]
+            yield test_config
+
+
+def main():
+    col_width = 21
+    labels = ["Test", "seed", "n_messages", "test_sleep"]
+    print("[=========]", "Gathering initialized.")
     print("[  Start  ]", "".join(num.rjust(col_width) for num in labels))
 
-    n = 0
-    for args in get_args(iterations):
-        print("[ Running ]", "".join(num.rjust(col_width)
-                                     for num in args[1:-1]))
-        n += 1
-        filename = "data/gather_" + \
-            "_".join(num for num in args[1:-1]) + ".out"
+    for args in get_expectmsg():
+        print("[ Running ]", "".join(num.rjust(col_width) for num in args))
+        filename = "data/ex_" + "_".join(num for num in args[1:]) + ".data"
         with open(filename, 'wb') as f:
             cp = subprocess.run(args, stdout=subprocess.PIPE)
             f.write(cp.stdout)
 
-        print("[  Done   ]", "{}/{}".format(n, 1024), n/1024 * 100, '%')
+    for args in get_unordered():
+        print("[ Running ]", "".join(num.rjust(col_width) for num in args))
+        filename = "data/un_" + "_".join(num for num in args[1:]) + ".data"
+        with open(filename, 'wb') as f:
+            cp = subprocess.run(args, stdout=subprocess.PIPE)
+            f.write(cp.stdout)
+
+    for args in get_expectmsg_sleep():
+        print("[ Running ]", "".join(num.rjust(col_width) for num in args))
+        filename = "data/exsl_" + "_".join(num for num in args[1:]) + ".data"
+        with open(filename, 'wb') as f:
+            cp = subprocess.run(args, stdout=subprocess.PIPE)
+            f.write(cp.stdout)
+
+    for args in get_unordered_sleep():
+        print("[ Running ]", "".join(num.rjust(col_width) for num in args))
+        filename = "data/unsl_" + "_".join(num for num in args[1:]) + ".data"
+        with open(filename, 'wb') as f:
+            cp = subprocess.run(args, stdout=subprocess.PIPE)
+            f.write(cp.stdout)
 
 
 if __name__ == '__main__':
-    iterations = 0
-    if len(sys.argv) > 1:
-        try:
-            iterations = int(sys.argv[1])
-        except:
-            print("usage: {} <num_iterations, integer>".format(
-                sys.argv[0]), file=sys.stderr)
-        main(iterations)
-    else:
-        print("usage: {} <num_iterations, integer>".format(
-            sys.argv[0]), file=sys.stderr)
+    main()
