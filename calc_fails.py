57f1f958709f4aead55f6b81700765dc96d783b1
Oskar Jansson <oskja067@student.liu.se>
2018-05-12 16:00:57 +0200

    updates: calc_fails output now tells percentage

--- calc_fails.py
+++ calc_fails.py
@@ -3,6 +3,7 @@ from sys import argv
 
 def main(filename):
     freq = {'exsl':0,'unsl':0,'ex':0,'un':0}
+    total = {'exsl':0,'unsl':0,'ex':0,'un':0}
     with open(filename, 'r') as f:
         skip_first = True
         for line in f:
@@ -10,9 +11,15 @@ def main(filename):
                 skip_first = False
                 continue
             l = line.split()
-            freq[l[0]] += 1 if l[4] == '1' else 0
+            freq[l[0]] += 1 if l[4] == '0' else 0
+            total[l[0]] += 1
 
     pp(freq)
+    pp(total)
+    for k,v in freq.items():
+        print("{} passed {}%".format(k,(((total[k] - v)/total[k])*100)))
+        print("{} failed {}%".format(k,((v/total[k])*100)))
+
 
 if __name__ == '__main__':
     if len(argv) < 1:
